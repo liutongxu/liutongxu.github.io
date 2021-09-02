daaffd4bce99b00889e31404fedfc07c65db27ca
刘桐序 <hiliutongxu@qq.com>
2021-09-02 18:49:35 +0800

    更新 waifu-tips.js

--- live2d/waifu-tips.js
+++ live2d/waifu-tips.js
@@ -77,7 +77,7 @@ function loadWidget(config) {
 			Live2D.captureFrame = true;
 		});
 		document.querySelector("#waifu-tool .fa-info-circle").addEventListener("click", () => {
-			open("https://github.com/stevenjoezhang/live2d-widget");
+			open("https://github.com/liutongxu/liutongxu.github.io");
 		});
 		document.querySelector("#waifu-tool .fa-times").addEventListener("click", () => {
 			localStorage.setItem("waifu-display", Date.now());
