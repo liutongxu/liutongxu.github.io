3e5f722776e4f8f5a4e6bf998146e073e5a21d45
刘桐序 <hiliutongxu@qq.com>
2021-09-03 10:50:18 +0800

    更新 waifu-tips.js

--- live2d/waifu-tips.js
+++ live2d/waifu-tips.js
@@ -132,7 +132,7 @@ function loadWidget(config) {
 		fetch("https://v1.hitokoto.cn")
 			.then(response => response.json())
 			.then(result => {
-				const text = `这句一言来自 <span>「${result.from}」</span>，是 <span>${result.creator}</span> 在 hitokoto.cn 投稿的。`;
+				const text = `这句一言来自 <span>「${result.from}」</span>，由 <span>${result.creator}</span> 投稿。`;
 				showMessage(result.hitokoto, 6000, 9);
 				setTimeout(() => {
 					showMessage(text, 4000, 9);
