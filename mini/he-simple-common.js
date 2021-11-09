24619ad3ce12573bb022f0a09a0ee0e054418d82
刘桐序 <hiliutongxu@qq.com>
2021-11-09 20:57:19 +0800

    -

---
+++ mini/he-simple-common.js
@@ -0,0 +1,10 @@
+(function (d) {
+  var c = d.createElement('link')
+  c.rel = 'stylesheet'
+  c.href = 'https://widget.heweather.net/simple/static/css/he-simple.css?v=1.4.0'
+  var s = d.createElement('script')
+  s.src = 'https://widget.heweather.net/simple/static/js/he-simple.js?v=1.4.0'
+  var sn = d.getElementsByTagName('script')[0]
+  sn.parentNode.insertBefore(c, sn)
+  sn.parentNode.insertBefore(s, sn)
+})(document)
